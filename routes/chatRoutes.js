73bd34822fc71b41c11abc7ae7e8ef7fa62df4ce
AwsKhader <aws.mohammed.khader@gmail.com>
2026-05-16 15:27:25 +0300

    Delete routes directory

--- routes/chatRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { chatReply } from "../controllers/chatController.js";
-
-const router = express.Router();
-
-router.post("/", chatReply);
-
-export default router;
